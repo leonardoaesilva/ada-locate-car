3917eec44671d7216a6f99d6f8bb8c194c41e23f
Leonardo Almeida <leonardoaesilva@outlook.com>
2023-02-14 00:37:52 -0300

    testes iniciais

--- src/Locadora.java
+++ src/Locadora.java
@@ -4,6 +4,13 @@ public class Locadora {
         BancoDeDados<PessoaJuridica> bancoDeClientesJuridicos = new OperadorDeDados<>();
         BancoDeDados<Veiculo> bancoDeVeiculos = new OperadorDeDados<>();
 
+        GerenciadorDeClientesFisicos clientesFisicos = new GerenciadorDeClientesFisicos(bancoDeClientesFisicos);
+        GerenciadorDeClientesJuridicos clientesJuridicos = new GerenciadorDeClientesJuridicos(bancoDeClientesJuridicos);
+        GerenciadorDeVeiculos veiculos = new GerenciadorDeVeiculos(bancoDeVeiculos);
+
+        clientesFisicos.adcCliente("Leo", "123");
+        System.out.println(clientesFisicos.listarTodos());
+
         System.out.println("Hello world!");
     }
 }
