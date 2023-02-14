be20f1074db60bd2e7c61d33bf4a53f2cb2a2491
Leonardo Almeida <leonardoaesilva@outlook.com>
2023-02-14 14:43:09 -0300

    menores alterações

--- src/TipoVeiculo.java
+++ src/TipoVeiculo.java
@@ -2,7 +2,7 @@ import java.math.BigDecimal;
 
 public enum TipoVeiculo {
     Pequeno(new BigDecimal("100.0")),
-    Médio(new BigDecimal("150.0")),
+    Medio(new BigDecimal("150.0")),
     SUV(new BigDecimal("200.0"));
 
     public BigDecimal valorAluguelDiario;
